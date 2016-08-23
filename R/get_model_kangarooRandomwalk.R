bf1fc5bb7033a9b47429f0c32117bf45c4a75aa9
stephaneshao <stephaneshao@g.harvard.edu>
2016-08-23 12:05:33 -0400

    sanity check for kangaroo model (random walk)

--- R/get_model_kangarooRandomwalk.R
+++ R/get_model_kangarooRandomwalk.R
@@ -4,19 +4,16 @@
 #'@export
 get_model_kangarooRandomwalk <- function(){
   model.kangarooRandomwalk = list()
-
   # dimension of parameter, observations, and states
   model.kangarooRandomwalk$dimtheta = 2
   model.kangarooRandomwalk$dimY = 2
   model.kangarooRandomwalk$dimX = 1
-
   # sampler from the prior distribution on parameters
   model.kangarooRandomwalk$rprior = function(Ntheta){
     sigma = runif(Ntheta,0,10)
     tau = runif(Ntheta,0,10)
     return (cbind(sigma,tau))
   }
-
   # density the prior distribution on parameters
   model.kangarooRandomwalk$dprior = function(theta, log = TRUE){
     sigma = theta[1]
@@ -29,19 +26,16 @@ get_model_kangarooRandomwalk <- function(){
     }
     return (combineterms(dunif(sigma,0,10,log),dunif(tau,0,10,log)))
   }
-
   # sampler from the initial distribution of the states
   model.kangarooRandomwalk$rinitial = function(theta,N=1){
     return (matrix(rlnorm(N,meanlog = 5,sdlog = sqrt(10)), nrow = N))
   }
-
   # sampler from the transition density of the states
   model.kangarooRandomwalk$rtransition = function(Xt,dt,theta){
     sigma = theta[1]
     N = nrow(Xt)
     return (matrix(Xt*exp(sigma*rnorm(N,mean = 0,sd = sqrt(dt))), nrow = N))
   }
-
   # density of the observations
   model.kangarooRandomwalk$dobs = function(Yt,Xt,t,theta,log = TRUE){
     tau = theta[2]
@@ -55,14 +49,12 @@ get_model_kangarooRandomwalk <- function(){
     return (combineterms(dnbinom(Yt[1],size = n,mu = Xt,log = log),
                          dnbinom(Yt[2],size = n,mu = Xt,log = log)))
   }
-
   # OPTIONAL: simulate observations
   model.kangarooRandomwalk$robs = function(Xt,t,theta){
     tau = theta[2]
     n = 1/tau
     return (rnbinom(2,size = n,mu = Xt))
   }
-
   # OPTIONAL: lower and upper bounds of observations
   model.kangarooRandomwalk$lower = c(0,0)
   model.kangarooRandomwalk$upper = c(Inf,Inf)
