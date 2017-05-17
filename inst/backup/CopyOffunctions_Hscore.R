b072ba6a2dbda257f4b9ca11279fadce7c58e622
stephaneshao <stephaneshao@g.harvard.edu>
2017-05-17 15:01:39 -0400

    create some backup files

---
+++ inst/backup/CopyOffunctions_Hscore.R
@@ -0,0 +1,162 @@
+#----------------------------------------------------------------------------------------#
+#---------------------------- CONTINUOUS OBSERVATIONS -----------------------------------#
+#----------------------------------------------------------------------------------------#
+# smc2 version when predictive density is intractable
+hincrementContinuous_smc2 = function(t,model,observationt,thetas,Wtheta,PFs,Ntheta) {
+  hincrement = 0
+  d1log = list()
+  d2log = list()
+  for (k in 1:model$dimY) {
+    Ek_theta = vector("numeric",Ntheta)
+    Fk_theta = vector("numeric",Ntheta)
+    for (m in 1:Ntheta){
+      Ek_theta[m] = 0
+      Fk_theta[m] = 0
+      if (Wtheta[m] == 0) {
+        next
+      }
+      PF = PFs[[m]]
+      X = PF$X
+      WX = PF$xnormW
+      if (is.null(dim(X))){
+        X = matrix(X,nrow = model$dimX)
+      }
+      # compute the derivatives for all k and all X at once during the first loop
+      if (k==1){
+        derivatives = model$derivativelogdobs(observationt,X,t,thetas[,m],model$dimY)
+        d1log[[m]] = derivatives$jacobian
+        d2log[[m]] = derivatives$hessiandiag
+      }
+      Ek_theta[m] = sum(WX*(d2log[[m]][,k] + (d1log[[m]][,k])^2))
+      Fk_theta[m] = sum(WX*d1log[[m]][,k])
+    }
+    Ek = sum(Wtheta*Ek_theta)
+    Fk = (sum(Wtheta*Fk_theta))^2
+    hincrement = hincrement + 2*Ek - Fk
+  }
+  return (hincrement)
+}
+#-------------------------------------------------------------------------------------------
+# smc version when predictive density is available
+hincrementContinuous_smc = function(t,model,observations,thetas,Wtheta,byproducts,Ntheta) {
+  hincrement = 0
+  d1log = list()
+  d2log = list()
+  for (k in 1:model$dimY) {
+    Ek_theta = array(0,dim = Ntheta)
+    Fk_theta = array(0,dim = Ntheta)
+    for (m in 1:Ntheta){
+      if (Wtheta[m] == 0) {
+        next
+      }
+      # compute the derivatives for all k at once during the first loop
+      if (k==1){
+        if (!is.null(byproducts)){
+          derivatives = model$derivativelogdpredictive(observations,t,thetas[,m],byproducts[[m]],model$dimY)
+        } else {
+          derivatives = model$derivativelogdpredictive(observations,t,thetas[,m],model$dimY)
+        }
+        d1log[[m]] = derivatives$jacobian
+        d2log[[m]] = derivatives$hessiandiag
+      }
+      Ek_theta[m] = d2log[[m]][k] + (d1log[[m]][k])^2
+      Fk_theta[m] = d1log[[m]][k]
+    }
+    Ek = sum(Wtheta*Ek_theta)
+    Fk = (sum(Wtheta*Fk_theta))^2
+    hincrement = hincrement + 2*Ek - Fk
+  }
+  return (hincrement)
+}
+
+#----------------------------------------------------------------------------------------#
+#------------------------------ DISCRETE OBSERVATIONS -----------------------------------#
+#----------------------------------------------------------------------------------------#
+# This function computes the approximation pt_hat(y) for a given set of particles
+phat_smc2 = function(t,model,y,thetas,thetanormw,Ntheta,Xpred,xprednormw) {
+  py = 0
+  for (m in 1:Ntheta){
+    if (thetanormw[m]==0){
+      next
+    }
+    py = py + thetanormw[m]*sum(xprednormw[,m]*model$dobs(y,Xpred[,,m],t,thetas[,m],log = FALSE))
+  }
+  return (py)
+}
+# This function computes the partial score term Hdk
+# a,b are vectors of componentwise lower and upper bounds of the observations
+Hdk = function(k,a,b,d,y,py_minusek,py,py_plusek) {
+  if (y[k]==b[k]) {
+    return (-2*(py-py_minusek)/py_minusek)
+  }
+  else {
+    if (y[k]==a[k]) {
+      return (2*(py_plusek-py)/py + ((py_plusek-py)/py)^2)
+    }
+    else {
+      return (2*((py_plusek-py)/py-(py-py_minusek)/py_minusek) + ((py_plusek-py)/py)^2)
+    }
+  }
+}
+# This function computes the partial score term Hd
+# a,b are vectors of componentwise lower and upper bounds of the observations
+# d is the dimension of y
+Hd_smc2 = function(t,model,yt,thetas,thetanormw,Ntheta,Xpred,xprednormw) {
+  a = model$lower
+  b = model$upper
+  d = model$dimY
+  result = 0
+  for (k in 1:d) {
+    ek = rep(0,d)
+    ek[k] = 1
+    py = phat_smc2(t,model,yt,thetas,thetanormw,Ntheta,Xpred,xprednormw)
+    py_minusek = phat_smc2(t,model,yt-ek,thetas,thetanormw,Ntheta,Xpred,xprednormw)
+    py_plusek = phat_smc2(t,model,yt+ek,thetas,thetanormw,Ntheta,Xpred,xprednormw)
+    result = result + Hdk(k,a,b,d,yt,py_minusek,py,py_plusek)
+  }
+  return (result)
+}
+#-------------------------------------------------------------------------------------------
+# smc version when predictive density is available
+# This function computes the approximation qt_hat(y) for a given set of particles
+phat_smc = function(t,model,observations,thetas,thetanormw,Ntheta,byproducts) {
+  py = 0
+  if (!is.null(byproducts)){
+    for (m in 1:Ntheta){
+      if (thetanormw[m]==0){
+        next
+      }
+      py = py + thetanormw[m]*model$dpredictive(observations,t,thetas[,m],byproducts[[m]],log = FALSE)
+    }
+  } else {
+    for (m in 1:Ntheta){
+      if (thetanormw[m]==0){
+        next
+      }
+      py = py + thetanormw[m]*model$dpredictive(observations,t,thetas[,m],log = FALSE)
+    }
+  }
+  return (py)
+}
+# This function computes the partial score term Hd
+# a,b are vectors of componentwise lower and upper bounds of the observations
+# d is the dimension of y
+Hd_smc = function(t,model,observations,thetas,thetanormw,Ntheta,byproducts) {
+  a = model$lower
+  b = model$upper
+  d = model$dimY
+  result = 0
+  for (k in 1:d) {
+    ek = rep(0,d)
+    ek[k] = 1
+    obsminusek = observations
+    obsplusek = observations
+    obsminusek[,t] = obsminusek[,t]-ek
+    obsplusek[,t] = obsplusek[,t]+ek
+    py = phat_smc(t,model,observations,thetas,thetanormw,Ntheta,byproducts)
+    py_minusek = phat_smc(t,model,obsminusek,thetas,thetanormw,Ntheta,byproducts)
+    py_plusek = phat_smc(t,model,obsplusek,thetas,thetanormw,Ntheta,byproducts)
+    result = result + Hdk(k,a,b,d,observations[,t],py_minusek,py,py_plusek)
+  }
+  return (result)
+}
--- inst/backup/CopyOfsmc_sampler.R
+++ inst/backup/CopyOfsmc_sampler.R
@@ -3,57 +3,68 @@
 #'@description This function runs the SMC algorithm, using tempering.
 #'@export
 smc_sampler <- function(observations, model, algorithmic_parameters){
-  Ntheta <- algorithmic_parameters$Ntheta
+  ## Set default values for the missing fields
+  algorithmic_parameters = set_default_algorithmic_parameters(observations, model, algorithmic_parameters)
+  model = set_default_model(model)
+  # Parse algorithmic parameters and set flags accordingly
+  nobservations = ncol(observations)
+  Ntheta = algorithmic_parameters$Ntheta
   nmoves = algorithmic_parameters$nmoves
   resampling = algorithmic_parameters$resampling
-  ess_objective <- algorithmic_parameters$ess_threshold*algorithmic_parameters$Ntheta
-  nobservations <- ncol(observations)
-  # Set flags for progress tracking
-  if (is.null(algorithmic_parameters$progress)) algorithmic_parameters$progress = FALSE
-  if (is.null(algorithmic_parameters$verbose)) algorithmic_parameters$verbose = FALSE
+  ess_objective = algorithmic_parameters$ess_threshold*algorithmic_parameters$Ntheta
+  # Monitor progress if needed
   if (algorithmic_parameters$progress) {
     print(paste("Started at:",Sys.time()))
     progbar = txtProgressBar(min = 0,max = nobservations,style=3)
-    count = 0
     time_start = proc.time()
   }
+  # Initialize empty arrays and lists to store the results
   ESS = array(NA,dim = c(nobservations)) #ESS at successive times t
   logevidence = array(NA,dim = c(nobservations)) #log-evidence at successive times t
-  rejuvenation_times <- c() #successive times where resampling is triggered
-  rejuvenation_accept_rate <- c() #successive acceptance rates of resampling
+  thetas_history = list() #successive sets of particles theta
+  normw_history = list() #successive sets of normalized weights for theta
   # if (is.null(algorithmic_parameters$rinitial_theta)){
   thetas <- model$rprior(Ntheta)
   # } else {
   # thetas <- algorithmic_parameters$rinitial_theta(Ntheta)
   # }
   # log target density evaluations at current particles
-  logtargetdensities <- apply(thetas, 1, model$dprior)
+  logtargetdensities <- apply(thetas, 2, model$dprior)
   # normalized weights
   normw <- rep(1/Ntheta, Ntheta)
   logw <- rep(0, Ntheta)
-  #
-  thetas_history <- list()
+
+  ########## if we start from a proposal instead of the prior (e.g. improper prior)
+  ########## then the weights should be initialized differently:
+  ########## log(prior_density) - log(proposal_density) ???
+
   thetas_history[[1]] <- thetas
-  normw_history <- list()
   normw_history[[1]] <- normw
-  #
-  logevidence <- rep(0, nobservations)
-  #
+  # initialize possible byproducts (e.g. Kalman filters, etc ...)
+  byproducts = list()
+  if (!is.null(model$initialize_byproducts)) {
+    for (itheta in 1:Ntheta){
+      byproducts[[itheta]] = model$initialize_byproducts(thetas[,itheta], observations)
+    }
+  } else {
+    byproducts = NULL
+  }
+  # assimilate observations one by one
   for (t in 1:nobservations){
     results <- assimilate_one_smc(thetas, t, observations, model, Ntheta, ess_objective,
                                   nmoves, resampling, logtargetdensities, logw, normw,
-                                  algorithmic_parameters$verbose)
+                                  byproducts, algorithmic_parameters$verbose)
     thetas <- results$thetas
     normw <- results$normw
     logw <- results$logw
     logtargetdensities <- results$logtargetdensities
     logevidence[t] <- results$logcst
+    if (!is.null(byproducts)) {byproducts = results$byproducts}
     thetas_history[[t+1]] <- thetas
     normw_history[[t+1]] <- normw
     # Update progress bar if needed
     if (algorithmic_parameters$progress) {
-      count = count + 1
-      setTxtProgressBar(progbar, count)
+      setTxtProgressBar(progbar, t)
     }
   }
   # Update progress bar if needed
@@ -64,6 +75,7 @@ smc_sampler <- function(observations, model, algorithmic_parameters){
     print(time_end)
   }
   return(list(thetas_history = thetas_history, normw_history = normw_history,
-              logevidence = cumsum(logevidence), logtargetdensities = logtargetdensities))
+              logevidence = cumsum(logevidence), logtargetdensities = logtargetdensities,
+              byproducts = byproducts))
 }
 
