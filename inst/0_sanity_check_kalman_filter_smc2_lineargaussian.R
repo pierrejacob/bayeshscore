ca2aaf3614cbe8a958999c231c37d6a7679e16f7
stephaneshao <stephaneshao@g.harvard.edu>
2017-05-17 02:19:42 -0400

    Added more sanity checks

--- inst/0_sanity_check_kalman_filter_smc2_lineargaussian.R
+++ inst/0_sanity_check_kalman_filter_smc2_lineargaussian.R
@@ -33,9 +33,9 @@ model$likelihood = NULL # this forces the use of SMC2
 model$dpredictive = NULL # this forces the use of SMC2
 smc2_results <- hscore(observations, model, algorithmic_parameters)
 
+#-------------------------------------------------------------------------------------------------
 # Plot the filtering means (and some particles X for one theta, e.g. the one with largest weight)
 xmean = rep(NA,nobservations)
-xmeanMAP = rep(NA,nobservations)
 sim = data.frame()
 indexMAP = which.max(smc2_results$normw_history[[nobservations+1]])
 Ntheta = algorithmic_parameters$Ntheta
@@ -43,27 +43,29 @@ for (t in 1:nobservations){
   PF = smc2_results$PF_history[[t+1]]
   normw = smc2_results$normw_history[[t+1]]
   xmean[t] = sum(sapply(1:Ntheta,function(i)sum(PF[[i]]$X*PF[[i]]$xnormW)*normw[i]))
-  xmeanMAP[t] = sum(PF[[indexMAP]]$X*PF[[indexMAP]]$xnormW)
   sim = rbind(sim, data.frame(X = c(PF[[indexMAP]]$X), w = c(PF[[indexMAP]]$xnormW), time = t))
 }
 
-# set parameters
-thetaMAP = smc2_results$thetas_history[[nobservations+1]][,indexMAP]
-phi = thetaMAP[1]
-sigmaW2 = thetaMAP[2]
-psi = thetaMAP[3]
-sigmaV2 = thetaMAP[4]
-initial_mean = 0
-initial_var = (sigmaW2)/(1-phi^2)
-# run Kalman filter
-KF = KF_filtering(Y,phi,psi,sigmaV2,sigmaW2,initial_mean,initial_var)
+# estimate (marginal) filtering means via KF (using the same particles thetas from SMC2)
+xmeanKF = rep(0,nobservations)
+for (t in 1:nobservations) {
+  normw = smc2_results$normw_history[[t+1]]
+  thetas = smc2_results$thetas_history[[t+1]]
+  for (i in 1:Ntheta){
+    phi = thetas[1,i]
+    sigmaW2 = thetas[2,i]
+    psi = thetas[3,i]
+    sigmaV2 = thetas[4,i]
+    initial_mean = 0
+    initial_var = (sigmaW2)/(1-phi^2)
+    KF = KF_filtering(observations[,1:t,drop=FALSE],phi,psi,sigmaV2,sigmaW2,initial_mean,initial_var)
+    xmeanKF[t] = xmeanKF[t] + KF[[t]]$muX_t_t*normw[i]
+  }
+}
 
-
+# plot
 ggplot() +
   geom_point(data=sim,aes(time,X,alpha=w)) +
-  # geom_line(aes(1:nobservations,X[,]),col='blue',linetype='dashed') +
-  # geom_line(aes(1:nobservations,Y[,]),col='blue',size=1) +
   geom_line(aes(1:nobservations,sapply(1:nobservations,function(t)KF[[t]]$muX_t_t)),col='black',size=1) +
-  geom_line(aes(1:nobservations,xmeanMAP),col='purple',size=2,linetype='dashed') +
-  theme(legend.position="none") +
-  ylab('particles X (dots), X (blue), Y (red)')
+  geom_line(aes(1:nobservations,xmeanKF),col='red',size=2,linetype='dashed') +
+  theme(legend.position="none")
